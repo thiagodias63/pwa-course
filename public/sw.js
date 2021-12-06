7e2a15c6ea3d49ba6b7f5e63381c3635066fc776
Thiago Dias <thidias6003@gmail.com>
2021-12-06 11:45:27 -0300

    modulo5: cache manual

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,24 @@
 self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installing Service worker', event)
+    event.waitUntil(
+        caches.open('static').then((cache) => {
+            console.log('[Service Worker] Precaching App Shell');
+            // cache.add('/src/js/app.js')
+            cache.addAll([
+                '/',
+                '/index.html',
+                '/src/js/app.js',
+                '/src/js/feed.js',
+                '/src/js/material.min.js',
+                'src/css/app.css',
+                'src/css/feed.css',
+                'src/images/main-image.jpg',
+                'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                'https://fonts.googleapis.com/icon?family=Material+Icons',
+                'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+            ])
+        })
+    )
 })
 
 self.addEventListener('activate', function(event) {
@@ -11,6 +30,14 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('fetch', function(event) {
     // console.log('[Service Worker] Fetching Service worker', event)
     // event.respondWith(fetch(event.request)); // overwrite the data that is send
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            // if is not found 
+            if (!response) return fetch(event.request);
+            // return the cached response
+            return response;
+        })
+    )
 })
 
 self.addEventListener('beforeinstallprompt', function(event) {

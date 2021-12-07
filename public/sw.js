e9fee6d569dcc662076e397c553c6ffc9eb93e78
Thiago Dias <thidias6003@gmail.com>
2021-12-07 08:57:36 -0300

    modulo5: feat: dynamic cache and version and cleaning

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,9 @@
+const CACHE_STATIC_NAME = 'static-'+Math.random();
+const CACHE_DYNAMIC_NAME = 'dynamic-'+Math.random();
 self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installing Service worker', event)
     event.waitUntil(
-        caches.open('static').then((cache) => {
+        caches.open(CACHE_STATIC_NAME).then((cache) => {
             console.log('[Service Worker] Precaching App Shell');
             // cache.add('/src/js/app.js')
             cache.addAll([
@@ -22,23 +24,48 @@ self.addEventListener('install', function(event) {
 })
 
 self.addEventListener('activate', function(event) {
-    console.log('[Service Worker] Activating Service worker...', event)
+    console.log('[Service Worker] Activating Service worker...', event);
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys.map(key => {
+                if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+                    console.log('[Service Worker] removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
     return self.clients.claim();
 })
+
+function dynamicCache (res, event) {
+    return caches.open(CACHE_DYNAMIC_NAME).then(
+        function(cache) {
+            cache.put(event.request.url, res.clone());
+            return res
+    })
+}
 
 // no life-cycle 
 self.addEventListener('fetch', function(event) {
     // console.log('[Service Worker] Fetching Service worker', event)
     // event.respondWith(fetch(event.request)); // overwrite the data that is send
     event.respondWith(
-        caches.match(event.request).then((response) => {
+        caches.match(event.request).then(function (response) {
             // if is not found 
-            if (!response) return fetch(event.request);
+            if (!response) {
+                return fetch(event.request)
+                    .then((res) => dynamicCache(res, event))
+                    .catch(err => {
+                        console.error(err)
+                    })
+            }
             // return the cached response
             return response;
         })
     )
 })
+
 
 self.addEventListener('beforeinstallprompt', function(event) {
     console.log('[Service Worker] Before install prompt', event);

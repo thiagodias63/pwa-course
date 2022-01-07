fe08e1511106b703fb2ab402654c7d235ce073cc
thiago dias <thidias6003@gmail.com>
2022-01-07 08:15:48 -0300

    module: push notifications

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,15 +1,103 @@
-var deferredPrompt;
+var deferredPrompt
+const enableNotificationsButtons = document.querySelectorAll(
+  ".enable-notifications"
+)
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("/sw.js")
     .then(() => console.log("sw registered"))
-    .catch((error) => console.log("sw error " + error));
+    .catch((error) => console.log("sw error " + error))
 }
 
 window.addEventListener("beforeinstallprompt", function (event) {
-  console.log("beforeinstallprompt fired");
-  event.preventDefault();
-  deferredPrompt = event;
-  return false;
-});
+  console.log("beforeinstallprompt fired")
+  event.preventDefault()
+  deferredPrompt = event
+  return false
+})
+
+function displayConfirmNotification() {
+  const options = {
+    body: "You successfuly subscribe to our Notification service!",
+    icon: "/src/images/icons/app-icon-96x96.png", // path to icon url
+    image: "/src/images/sf-boat.jpg", // part of the content of notification
+    dir: "ltr", // notification direction
+    lang: "en-US", // language
+    vibrate: [100, 50, 200], // vibration of device
+    badge: "/src/images/icons/app-icon-96x96.png",
+    tag: "confirm-notification", // id for notification
+    renotify: true, // if true, same tag can vibrate again
+    actions: [
+      // não funciona em todos os devices, não colocar as principais funcionalidades aqui
+      {
+        action: "confirm",
+        title: "Okay",
+        icon: "/src/images/icons/app-icon-96x96.png",
+      },
+      {
+        action: "cancel",
+        title: "Cancel",
+        icon: "/src/images/icons/app-icon-96x96.png",
+      },
+    ],
+  }
+  if ("serviceWorker" in navigator) {
+    return navigator.serviceWorker.ready.then((serviceWorkerRegistered) => {
+      serviceWorkerRegistered.showNotification(
+        "Successfully subscribed! (From SW)",
+        options
+      )
+    })
+  }
+  new Notification("Successfully subscribed!", options)
+}
+
+function configurePushSubscriptions() {
+  let registration
+  navigator.serviceWorker.ready
+    .then((serviceWorkerRegistered) => {
+      registration = serviceWorkerRegistered
+      return serviceWorkerRegistered.pushManager.getSubscription()
+    })
+    .then((subscription) => {
+      if (subscription === null) {
+        // create a new
+        const vapidPublicKey = "" // the web-push public key
+        registration.pushManager.subscribe({
+          userVisibleOnly: true,
+        })
+      } else {
+        // update the subscription
+      }
+    })
+}
+
+function askForNotificiationPermission(event) {
+  Notification.requestPermission((result) => {
+    console.log("User Choice ", result)
+    if (result !== "granted") {
+      console.log("No notification permission granted")
+      return
+    }
+    configurePushSubscriptions()
+    // displayConfirmNotification()
+    event.target.style.display = "none"
+  })
+  // .then((response) => {
+  //   console.log(response)
+  // })
+  // .catch((error) => {
+  //   console.error(error)
+  // })
+}
+
+if ("Notification" in window && "serviceWorker" in navigator) {
+  enableNotificationsButtons.forEach((enableNotificationButton) => {
+    enableNotificationButton.style.display = "inline-block"
+    enableNotificationButton.addEventListener(
+      "click",
+      askForNotificiationPermission
+    )
+  })
+}
--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,9 @@
-importScripts("/src/js/idb.js");
-importScripts("/src/js/idb-store.js");
-const url = "https://ficha-academia-web-top.firebaseio.com/posts.json";
-const cache_version = 9;
-const CACHE_STATIC_NAME = "static-" + 3;
-const CACHE_DYNAMIC_NAME = "dynamic-" + cache_version;
+importScripts("/src/js/idb.js")
+importScripts("/src/js/idb-store.js")
+const url = "https://ficha-academia-web-top.firebaseio.com/posts.json"
+const cache_version = 9
+const CACHE_STATIC_NAME = "static-" + 3
+const CACHE_DYNAMIC_NAME = "dynamic-" + cache_version
 const STATIC_FILES = [
   "/",
   "/index.html",
@@ -19,19 +19,19 @@ const STATIC_FILES = [
   "https://fonts.googleapis.com/css?family=Roboto:400,700",
   "https://fonts.googleapis.com/icon?family=Material+Icons",
   "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
-];
+]
 
 function trimCache(cacheName, maxItems) {
   caches
     .open(cacheName)
     .then(function (cache) {
-      return cache.keys();
+      return cache.keys()
     })
     .then(function (keys) {
       if (keys.length > maxItems) {
-        cache.delete(keys[0]).then(trimCache(cacheName, maxItems));
+        cache.delete(keys[0]).then(trimCache(cacheName, maxItems))
       }
-    });
+    })
 }
 
 const sendData = async (post) => {
@@ -47,51 +47,51 @@ const sendData = async (post) => {
         image:
           "https://firebasestorage.googleapis.com/v0/b/ficha-academia-web-top.appspot.com/o/sf-boat.jpg?alt=media&token=313e7833-2f83-44ac-a005-3b8ec33f26b4",
       }),
-    });
-    console.log("[Service Worker] Send Data", response);
+    })
+    console.log("[Service Worker] Send Data", response)
     if (response.ok) {
-      console.log(post.id);
-      clearItemFromData(post.id, "sync-posts");
+      console.log(post.id)
+      clearItemFromData(post.id, "sync-posts")
     }
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-};
+}
 
 self.addEventListener("install", function (event) {
-  console.log("[Service Worker] Installing Service worker", event);
+  console.log("[Service Worker] Installing Service worker", event)
   event.waitUntil(
     caches.open(CACHE_STATIC_NAME).then((cache) => {
-      console.log("[Service Worker] Precaching App Shell");
+      console.log("[Service Worker] Precaching App Shell")
       // cache.add('/src/js/app.js')
       // App Shell
-      cache.addAll(STATIC_FILES);
+      cache.addAll(STATIC_FILES)
     })
-  );
-});
+  )
+})
 
 self.addEventListener("activate", function (event) {
-  console.log("[Service Worker] Activating Service worker...", event);
+  console.log("[Service Worker] Activating Service worker...", event)
   event.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
         keys.map((key) => {
           if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-            console.log("[Service Worker] removing old cache", key);
-            return caches.delete(key);
+            console.log("[Service Worker] removing old cache", key)
+            return caches.delete(key)
           }
         })
-      );
+      )
     })
-  );
-  return self.clients.claim();
-});
+  )
+  return self.clients.claim()
+})
 
 function dynamicCache(res, event) {
   return caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
-    cache.put(event.request.url, res.clone());
-    return res;
-  });
+    cache.put(event.request.url, res.clone())
+    return res
+  })
 }
 
 // no life-cycle
@@ -106,20 +106,20 @@ function cacheWithNetworkFallback(event) {
         return fetch(event.request)
           .then((res) => dynamicCache(res, event))
           .catch((err) => {
-            console.error(err);
+            console.error(err)
             return caches.open(CACHE_STATIC_NAME).then(function (cache) {
               if (
                 event.request.url.headers.get("accept").includes("text/html")
               ) {
-                return cache.match("/offline.html");
+                return cache.match("/offline.html")
               }
-            });
-          });
+            })
+          })
       }
       // return the cached response
-      return response;
+      return response
     })
-  );
+  )
 }
 
 // Strategy: cache with cache only
@@ -129,14 +129,14 @@ function cacheWithCacheOnly(event) {
   event.respondWith(
     // return the cached response
     caches.match(event.request)
-  );
+  )
 }
 
 // Strategy: network only
 function networkOnly(event) {
   // console.log('[Service Worker] Fetching Service worker', event)
   // event.respondWith(fetch(event.request)); // overwrite the data that is send
-  event.respondWith(fetch(event.request));
+  event.respondWith(fetch(event.request))
 }
 
 // Strategy: network with cache fallback
@@ -145,28 +145,28 @@ function networkWithCacheFallback(event) {
     fetch(event.request)
       .then((res) => dynamicCache(res, event))
       .catch(function (err) {
-        caches.match(event.request);
+        caches.match(event.request)
       })
-  );
+  )
 }
 
 // Stategy: cache then network
 function cacheThenNetwork(event) {
-  var url = "https://ficha-academia-web-top.firebaseio.com/posts";
+  var url = "https://ficha-academia-web-top.firebaseio.com/posts"
   if (event.request.url.indexOf(url) > -1) {
     event.respondWith(
       fetch(event.request).then(function (response) {
-        var clonedResponse = response.clone();
+        var clonedResponse = response.clone()
         clearAllData()
           .then(() => {
-            return clonedResponse.json();
+            return clonedResponse.json()
           })
           .then((data) => {
             for (let key in data) {
-              writeData(data[key]);
+              writeData(data[key])
             }
-          });
-        return response;
+          })
+        return response
       })
       // version with cache
       // caches.open(CACHE_DYNAMIC_NAME)
@@ -179,33 +179,83 @@ function cacheThenNetwork(event) {
       //     return response;
       // })
       // })
-    );
+    )
   } else if (STATIC_FILES.includes(event.request.url)) {
     // Strategy: cache with cache only
-    cacheWithCacheOnly(event);
+    cacheWithCacheOnly(event)
   } else {
     // Strategy: cache Fwith network fallback
-    cacheWithNetworkFallback(event);
+    cacheWithNetworkFallback(event)
   }
 }
 
 self.addEventListener("beforeinstallprompt", function (event) {
-  console.log("[Service Worker] Before install prompt", event);
-});
+  console.log("[Service Worker] Before install prompt", event)
+})
 
-self.addEventListener("fetch", cacheThenNetwork);
+self.addEventListener("fetch", cacheThenNetwork)
 
 self.addEventListener("sync", function (event) {
-  console.log("[Service Worker] Background syncing", event);
+  console.log("[Service Worker] Background syncing", event)
   if (event.tag == "sync-new-posts") {
-    console.log("[Service Worker] Syncing new posts");
+    console.log("[Service Worker] Syncing new posts")
     event.waitUntil(
       readData("sync-posts").then(function (posts) {
-        console.log({ posts });
+        console.log({ posts })
         for (post of posts) {
-          sendData(post);
+          sendData(post)
         }
       })
-    );
+    )
   }
-});
+})
+
+self.addEventListener("notificationclick", function (event) {
+  const notification = event.notification
+  const action = event.action
+
+  console.log(notification)
+
+  if (action === "confirm") {
+    console.log("Confirm was chosen")
+    notification.close()
+  } else {
+    event.waitUntil(
+      clients.matchAll().then((clients) => {
+        const clientFound = clients.find(
+          (client) => client.visibilityState === "visible"
+        )
+        if (clientFound) {
+          clientFound.navigate("http://localhost:8080" + notification.data.url)
+          clientFound.focus()
+        } else {
+          clients.openWindow("http://localhost:8080" + notification.data.url)
+        }
+      })
+    )
+    notification.close()
+    console.log(action)
+  }
+})
+
+self.addEventListener("notificationclose", function (event) {
+  console.log("Notification was closed", event)
+})
+
+self.addEventListener("push", function (event) {
+  console.log("Push Notification received", event)
+  let data = {
+    title: "New!",
+    content: "Something new happend!",
+    openUrl: "/help",
+  }
+  if (event.data) data = JSON.parse(event.data.text())
+  const options = {
+    body: data.content,
+    icon: "/src/images/icons/app-icon-96x96.png",
+    badge: "/src/images/icons/app-icon-96x96.png",
+    data: { url: data.openUrl },
+  }
+
+  event.waitUntil(self.registration.showNotification(data.title, options))
+})
